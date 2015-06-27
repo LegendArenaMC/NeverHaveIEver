696753af10e036abf7be064c60653022b8c9d366
ThePixelDev <6title78cp@gmail.com>
2015-06-27 11:48:24 -0600

    I promise, NHIE will eventually get done
    ..okay even I can't keep a straight face saying that

---
+++ src/main/java/legendarena/NeverHaveIEver.kt
@@ -0,0 +1,16 @@
+package legendarena
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class NeverHaveIEver : JavaPlugin() {
+
+    override fun onEnable() {
+        //
+    }
+
+    override fun onDisable() {
+        //
+    }
+
+}
+
